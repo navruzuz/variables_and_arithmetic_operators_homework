4dbfb991e141cef7e748d9bb60fe5276a5a21d88
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-07-14 17:31:32 +0500

    bajarildi

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,5 +1,52 @@
-# Create a variable "a"  and assign it the value of the int 3.
-
-# Create a variable "b"  and assign it the value of the int 4.
-
-# Create a variable called 'answer' and assign it the sum of a and b.
+#1-kod
+#def main(a,b,c):
+#    if (a>b):
+#       if a>c:
+#            print(a)
+#        else:
+#           print(c)    
+#    else:
+#        if b>c:
+#            print(b)    
+#       else:
+#            print(c)
+#print(main(2,8,6))     
+#2-kod      
+# x=6
+# if 1==x:
+#     print("one")
+# if 2==x:
+#     print("two")
+# if 3==x:
+#     print("three")
+# if 4==x:
+#     print("fo")
+# if 5==x:
+#     print('open',"five")
+# if 6==x:
+#     print('open',"six")
+# if 7==x:
+#     print('open',"seven")
+# if 8==x:
+#     print('open',"eight")
+# if 9==x:
+#     print('open',"nine")
+x=6
+if 1==x:
+    print("one")
+elif 2==x:
+    print("two")
+elif 3==x:
+    print("three")
+elif 4==x:
+    print("fo")
+elif 5==x:
+    print('open',"five")
+elif 6==x:
+    print('open',"six")
+elif 7==x:
+    print('open',"seven")
+elif 8==x:
+    print('open',"eight")
+elif 9==x:
+    print('open',"nine")
