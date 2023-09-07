1943db0ec98d9d0c87175de9265fd07b24137e15
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-09-07 09:05:32 +0500

    bajarildi

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,9 +1,9 @@
 # Create a variable "a"  and assign it the value of the int 7.
-a=7
+a=10
 # Create a variable "b"  and assign it the value of the int 3.
-b=3
+b=8
 # Create a variable "c"  and assign it the value of the int 5.
-c=5
+c=10
 # Create a variable called "answer" and assign it the sum of a, b and c
 answer=a+b+c
 print(answer)
