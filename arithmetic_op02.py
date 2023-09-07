f29abf2441eb6c3aacf1abec23d6bfab8c51f9cb
navruzuz <hamrayevnavruzbek9906@gmail.com>
2023-09-07 09:01:25 +0500

    bajarildi

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,36 +1,3 @@
-#1-kod
-#def main(a,b,c):
-#    if (a>b):
-#       if a>c:
-#            print(a)
-#        else:
-#           print(c)    
-#    else:
-#        if b>c:
-#            print(b)    
-#       else:
-#            print(c)
-#print(main(2,8,6))     
-#2-kod      
-# x=6
-# if 1==x:
-#     print("one")
-# if 2==x:
-#     print("two")
-# if 3==x:
-#     print("three")
-# if 4==x:
-#     print("fo")
-# if 5==x:
-#     print('open',"five")
-# if 6==x:
-#     print('open',"six")
-# if 7==x:
-#     print('open',"seven")
-# if 8==x:
-#     print('open',"eight")
-# if 9==x:
-#     print('open',"nine")
 x=6
 if 1==x:
     print("one")
